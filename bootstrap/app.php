b1195e416784b9c29e76f0f6aee6d155e1f4bf33
Zwar <izwarali22@gmail.com>
2025-10-27 19:07:37 +0700

    Fix CSRF and session for Vercel - resolve page expired error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
-        // Disable TrustHosts for Vercel - allow all hosts
+        $middleware->trustProxies(at: '*', headers: 
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
+        
+        // Allow Vercel domains for CSRF
+        $middleware->validateCsrfTokens(except: [
+            // Add any routes that should be excluded from CSRF verification
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
